f4c72363267fb561f6f6a95bc1574a151e1644f1
awongh <akira@awongh.com>
2018-06-20 13:32:22 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const myFunction = require('./my-require')
+
+console.log( myFunction.add(6, 4) );
+
+var cats = require('cat-ascii-faces')
+
+console.log( cats() );
+
+
+// console.log("hellow world");
+
+
+// var foo = 2;
+// console.log( foo * 100 );
+
+// console.log( process.argv[0]);
+// console.log( process.argv[1]);
+// console.log( process.argv[2]);
+
+// console.log( process.env.PATH );
+
+
+
+
